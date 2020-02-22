aedd35e7652bf0a21832d77426cf2c54e115ab78
Naveen <naveenprasadsomasundaram@gmail.com>
2020-02-23 00:38:50 +0530

    resolving errors in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,url_for,request, json, jsonify
-import pandas as pd 
+import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -21,84 +21,48 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	if request.method == 'POST':
-		message = re.sub('[^A-Za-z0-9]+', ' ',request.form['message'])
-		username = request.form['username']
+    if request.method == 'POST':
+        message = re.sub('[^A-Za-z0-9]+', ' ',request.form['message'])
+        username = request.form['username']
 
-		data = [message]
+        data = [message]
 
-		vect = cv.transform(data).toarray()
-		my_prediction = clf.predict(vect)
-		print(my_prediction[0])
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+        print(my_prediction[0])
 
         fmt = "%Y-%m-%d %H:%M:%S %Z%z"
-		now_utc = datetime.now(timezone('UTC'))
-        now_india = now_pacific.astimezone(timezone('Asia/Kolkata''))
-        now = now_berlin.strftime(fmt)
+        now_utc = datetime.now(timezone('UTC'))
+        now_india = now_utc.astimezone(timezone('Asia/Kolkata'))
+        now = now_india.strftime(fmt)
 
-        
-        
-		jsonfilename = "client_data/" + username + ".json"
-		file = pathlib.Path(jsonfilename)
-		previous_data = []
+        jsonfilename = "client_data/" + username + ".json"
+        file = pathlib.Path(jsonfilename)
+        previous_data = []
 
-		# reading json file..
-		if file.exists():
-			with open(jsonfilename) as f:
-				d = json.load(f)
-				previous_data = (d)
+        # reading json file..
+        if file.exists():
+            with open(jsonfilename) as f:
+                d = json.load(f)
+                previous_data = (d)
 
 
-		# filename = os.path.join(app.static_folder, jsonfilename)
-		response = {
-			"username": username,
-			"prediction": str(my_prediction[0]),
-			"date": now,
-			"text": message
-		}
+        response = { "username": username,"prediction": str(my_prediction[0]),"date": now,"text": message}
+        previous_data.append(response)
 
-		previous_data.append(response)
-		'''
-		else :
-			for key in previous_data:
-				key.append(response)
-		'''
+        # write data into file..
+        with open(jsonfilename, 'w') as f:
+            json.dump(previous_data, f)
 
-		# previous_data.append(response)
+        print("completed.")
+        response = { "username": username, "response": previous_data}
 
-		# write data into file..
-		with open(jsonfilename, 'w') as f:
-			json.dump(previous_data, f)
-
-		print("completed.")
-		'''
-		basedir = os.path.realpath(os.path.dirname(__file__))
-		data_file = os.path.join(basedir, 'static/data', 'sample.json')
-		jsondata = json.load(open(data_file))
-		
-
-		clientData = []
-		json_data = open(os.path.join("static", "data", "sample.json"), "r")
-
-		clientData.append(json_data)
-		jsonStr = json.dumps(clientData)
-		print("asdas: ", jsonify(Employees=jsonStr))
-		'''
-		response = {
-			"username": username,
-			"response": previous_data
-		}
-		# print(response)
-
-
-		# response = json.loads(response)
-	return render_template('result.html',prediction = response)
-
-
+        # response = json.loads(response)
+        return render_template('result.html',prediction = response)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
